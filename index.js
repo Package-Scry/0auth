05e2f35ed1b754faab544d2c5f300fbd6a174e84
Kristof Szollosi <kristof.szollosi92+git@gmail.com>
2021-02-13 23:07:36 +0100

    add basic login

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+app.get("/", async (req, res) =>
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}`)
+);
+
+app.get("/auth/github/callback", async (req, res) => {
+  const body = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    code: req.query.code,
+  };
+  const opts = { headers: { accept: "application/json" } };
+  
+  try {
+    const response = await axios.post(
+      `https://github.com/login/oauth/access_token`,
+      body,
+      opts
+    );
+    const token = response.data.access_token;
+
+    res.json("success");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("App listening on port " + port));
