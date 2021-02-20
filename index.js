3cbba574e0a7c00dff5e56b76f7a900768aac701
Kristof Szollosi <kristof.szollosi92+git@gmail.com>
2021-02-20 18:27:14 +0100

    add cookie key

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.use(
     secret: process.env.REDIS_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      secure: false, // if true only transmit cookie over https
+      httpOnly: false, // if true prevent client side JS from reading the cookie 
+      maxAge: 1000 * 60 * 10 // session max age in miliseconds
+  }
   })
 );
 
