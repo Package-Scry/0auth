dfaea0ec73df57fc8526079aad9ca35d3c99f594
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-06-09 23:32:43 +0200

    move app to separate file

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const http = require("http");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+const URI = process.env.MONGO_URI;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const CORS_ORIGIN = ["https://www.packagescry.com", "https://github.com"];
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.enable("trust proxy");
+app.use(bodyParser.json());
+app.use(cors({ origin: CORS_ORIGIN, credentials: true }));
+app.use(
+  session({
+    secret: CLIENT_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      sameSite: "none",
+      maxAge: 14 * 24 * 60 * 60,
+      secure: true,
+    },
+    store: MongoStore.create({
+      mongoUrl: URI,
+      ttl: 14 * 24 * 60 * 60,
+      touchAfter: 24 * 60 * 60,
+    }),
+  })
+);
+
+module.exports = { app, server };
