ce3ef87ef5b846469c1ca350f9e9e4a8f94e4cfe
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-07-03 17:17:03 +0200

    revert test changes

--- payment/routes.js
+++ payment/routes.js
@@ -6,8 +6,7 @@ const {
   createEvent,
 } = require("./utils")
 const express = require("express")
-const STRIPE_API_KEY = process.env.STRIPE_TEST_KEY
-const stripe = require("stripe")(STRIPE_API_KEY)
+
 module.exports = () => {
   app.post("/post/create-subscription", authenticate, async (req, res) => {
     const { id } = res.locals?.user
@@ -55,48 +54,55 @@ module.exports = () => {
     express.raw({ type: "application/json" }),
     async (req, res) => {
       console.log("STRIPE WEBHOOK")
-      let event = req.body
-      // Only verify the event if you have an endpoint secret defined.
-      // Otherwise use the basic event deserialized with JSON.parse
-      const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET || ""
-      console.log({ endpointSecret })
-      if (endpointSecret) {
-        // Get the signature sent by Stripe
-        const signature = req.headers["stripe-signature"]
-        console.log(signature)
-        try {
-          event = stripe.webhooks.constructEvent(
-            req.body,
-            signature,
-            endpointSecret
-          )
-        } catch (err) {
-          console.log(`⚠️  Webhook signature verification failed.`, err.message)
-          return res.sendStatus(400)
-        }
-      }
+      const event = createEvent(req, res)
 
-      // Handle the event
-      switch (event.type) {
-        case "payment_intent.succeeded":
-          const paymentIntent = event.data.object
-          console.log(
-            `PaymentIntent for ${paymentIntent.amount} was successful!`
-          )
-          // Then define and call a method to handle the successful payment intent.
-          // handlePaymentIntentSucceeded(paymentIntent);
+      // Extract the object from the event.
+      const dataObject = event?.data?.object
+
+      switch (event?.type) {
+        case "invoice.payment_succeeded":
+          if (dataObject.billing_reason === "subscription_create") {
+            console.log("PAYMENT SUCCEEDED")
+            console.log(JSON.stringify(dataObject, null, 2))
+            // save hasPro to db
+            // const subscription_id = dataObject.subscription
+            // const payment_intent_id = dataObject.payment_intent
+
+            // Retrieve the payment intent used to pay the subscription
+            // const payment_intent = await stripe.paymentIntents.retrieve(
+            //   payment_intent_id
+            // )
+
+            // const subscription = await stripe.subscriptions.update(
+            //   subscription_id,
+            //   {
+            //     default_payment_method: payment_intent.payment_method,
+            //   }
+            // )
+          }
           break
-        case "payment_method.attached":
-          const paymentMethod = event.data.object
-          // Then define and call a method to handle the successful attachment of a PaymentMethod.
-          // handlePaymentMethodAttached(paymentMethod);
+        case "invoice.paid":
+          // save to db
+          console.log("INVOICE PAID")
+          console.log(JSON.stringify(dataObject, null, 2))
+          break
+        case "invoice.payment_failed":
+          // warn
+          console.log("PAYMENT FAILED")
+          console.log(JSON.stringify(dataObject, null, 2))
+          break
+        case "customer.subscription.deleted":
+          console.log("SUB DELETED")
+          console.log(JSON.stringify(dataObject, null, 2))
+          if (event.request != null) {
+            // remove from db -- unsubbed
+          } else {
+            // remove from db -- unsubbed automatically
+          }
           break
         default:
-          // Unexpected event type
-          console.log(`Unhandled event type ${event.type}.`)
+        // Unexpected event type
       }
-
-      // Return a 200 res to acknowledge receipt of the event
       res.send()
     }
   )
