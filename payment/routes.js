798d78e635e07ca52255deb43c6313fc799c8a1a
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-07-03 16:55:48 +0200

    test webhook example

--- payment/routes.js
+++ payment/routes.js
@@ -6,7 +6,8 @@ const {
   createEvent,
 } = require("./utils")
 const express = require("express")
-
+const STRIPE_API_KEY = process.env.STRIPE_TEST_KEY
+const stripe = require("stripe")(STRIPE_API_KEY)
 module.exports = () => {
   app.post("/post/create-subscription", authenticate, async (req, res) => {
     const { id } = res.locals?.user
@@ -54,56 +55,23 @@ module.exports = () => {
     express.raw({ type: "application/json" }),
     async (req, res) => {
       console.log("STRIPE WEBHOOK")
-      const event = createEvent(req, res)
+      const sig = req.headers["stripe-signature"]
 
-      // Extract the object from the event.
-      const dataObject = event?.data?.object
+      let event
 
-      switch (event?.type) {
-        case "invoice.payment_succeeded":
-          if (dataObject.billing_reason === "subscription_create") {
-            console.log("PAYMENT SUCCEEDED")
-            console.log(JSON.stringify(dataObject, null, 2))
-            // save hasPro to db
-            // const subscription_id = dataObject.subscription
-            // const payment_intent_id = dataObject.payment_intent
-
-            // Retrieve the payment intent used to pay the subscription
-            // const payment_intent = await stripe.paymentIntents.retrieve(
-            //   payment_intent_id
-            // )
-
-            // const subscription = await stripe.subscriptions.update(
-            //   subscription_id,
-            //   {
-            //     default_payment_method: payment_intent.payment_method,
-            //   }
-            // )
-          }
-          break
-        case "invoice.paid":
-          // save to db
-          console.log("INVOICE PAID")
-          console.log(JSON.stringify(dataObject, null, 2))
-          break
-        case "invoice.payment_failed":
-          // warn
-          console.log("PAYMENT FAILED")
-          console.log(JSON.stringify(dataObject, null, 2))
-          break
-        case "customer.subscription.deleted":
-          console.log("SUB DELETED")
-          console.log(JSON.stringify(dataObject, null, 2))
-          if (event.request != null) {
-            // remove from db -- unsubbed
-          } else {
-            // remove from db -- unsubbed automatically
-          }
-          break
-        default:
-        // Unexpected event type
+      try {
+        event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret)
+      } catch (err) {
+        // On error, log and return the error message
+        console.log(`❌ Error message: ${err.message}`)
+        return res.status(400).send(`Webhook Error: ${err.message}`)
       }
-      res.send()
+
+      // Successfully constructed event
+      console.log("✅ Success:", event.id)
+
+      // Return a response to acknowledge receipt of the event
+      res.json({ received: true })
     }
   )
 }
