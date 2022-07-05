3c9f132313b51bda7ebb9303b4b3c9d3b68c33d0
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-07-05 21:52:03 +0200

    format file

--- auth/index.js
+++ auth/index.js
@@ -1,38 +1,38 @@
-const { ObjectId } = require("mongodb");
-const jwt = require("jsonwebtoken");
+const { ObjectId } = require("mongodb")
+const jwt = require("jsonwebtoken")
 
-const io = require("../socket");
-const { getCurrentUser } = require("../controllers");
+const io = require("../socket")
+const { getCurrentUser } = require("../controllers")
 
 const signJWT = (idUser) =>
-  jwt.sign({ id: idUser }, process.env.SECRET, { expiresIn: "7d" });
+  jwt.sign({ id: idUser }, process.env.SECRET, { expiresIn: "7d" })
 
 module.exports = {
   authenticate: async (req, res, next) => {
-    const idUser = req.session?.user?.id;
+    const idUser = req.session?.user?.id
 
-    if (!idUser) res.json({ status: "failed", user: null });
+    if (!idUser) res.json({ status: "failed", user: null })
     else {
-      const user = await getCurrentUser({ _id: ObjectId(idUser) });
+      const user = await getCurrentUser({ _id: ObjectId(idUser) })
 
       if (!user) {
         req.session.destroy((error) => {
-          if (error) console.warn(error);
+          if (error) console.warn(error)
 
-          res.json({ status: "failed", user: null });
-        });
+          res.json({ status: "failed", user: null })
+        })
       } else {
-        const { _id, username } = user;
+        const { _id, username } = user
 
-        res.locals.user = { id: _id, username };
+        res.locals.user = { id: _id, username }
 
-        next();
+        next()
       }
     }
   },
   authenticateWithSocket: (idSocket, idUser, hasPro) => {
-    const JWT = signJWT(idUser);
+    const JWT = signJWT(idUser)
 
-    io.to(idSocket).emit("authentication", { token: JWT, hasPro });
+    io.to(idSocket).emit("authentication", { token: JWT, hasPro })
   },
-};
+}
