c1d50343668ce87e725a592833e168d0a4bddb86
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-07-05 19:52:16 +0200

    add metadata to subscription too

--- payment/utils.js
+++ payment/utils.js
@@ -27,6 +27,9 @@ module.exports = {
         ],
         subscription_data: {
           trial_period_days: TRIAL_AMOUNT_DAYS,
+          metadata: {
+            idUser: idUser.toString(),
+          },
         },
         // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
         // the actual Session ID is returned in the query parameter when your customer
