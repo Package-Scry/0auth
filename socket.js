971c49b84bdf9b333e7b8001385a8c00f2fb6f2a
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-07-09 13:19:48 +0200

    add cors to socket io

--- socket.js
+++ socket.js
@@ -1,4 +1,9 @@
-const { server } = require("./app");
-const io = require("socket.io")(server);
+const { server } = require("./app")
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "https://www.packagescry.com",
+    methods: ["GET", "POST"],
+  },
+})
 
-module.exports = io;
+module.exports = io
