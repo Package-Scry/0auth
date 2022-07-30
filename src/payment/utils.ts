aa669b66b3d2b00453c71006f606fa02b2aa44e8
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-07-30 15:14:04 +0200

    convert exports to es6

--- src/payment/utils.ts
+++ src/payment/utils.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express"
 import { ObjectId } from "mongodb"
 
 const STRIPE_MONTHLY_ID = "price_1KsZ4ZEbki2GiZihrbfz68np"
-const STRIPE_YEARLY_ID = "price_1KsZ4ZEbki2GiZihRGuM1sPR"
+export const STRIPE_YEARLY_ID = "price_1KsZ4ZEbki2GiZihRGuM1sPR"
 const TRIAL_AMOUNT_DAYS = 30
 const stripe = new Stripe(STRIPE_API_KEY, {
   apiVersion: "2020-08-27",
@@ -19,100 +19,98 @@ type CommonError = {
   message: string
 }
 
-module.exports = {
-  STRIPE_YEARLY_ID,
-  checkout: async (idUser: string, period: PlanPeriods) => {
-    const idPrice =
-      period === PlanPeriods.Monthly ? STRIPE_MONTHLY_ID : STRIPE_YEARLY_ID
+export const checkout = async (idUser: string, period: PlanPeriods) => {
+  const idPrice =
+    period === PlanPeriods.Monthly ? STRIPE_MONTHLY_ID : STRIPE_YEARLY_ID
 
-    try {
-      const session = await stripe.checkout.sessions.create({
-        mode: "subscription",
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      metadata: {
+        idUser: idUser.toString(),
+      },
+      line_items: [
+        {
+          price: idPrice,
+          quantity: 1,
+        },
+      ],
+      subscription_data: {
+        trial_period_days: TRIAL_AMOUNT_DAYS,
         metadata: {
           idUser: idUser.toString(),
         },
-        line_items: [
-          {
-            price: idPrice,
-            quantity: 1,
-          },
-        ],
-        subscription_data: {
-          trial_period_days: TRIAL_AMOUNT_DAYS,
-          metadata: {
-            idUser: idUser.toString(),
-          },
-        },
-        // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
-        // the actual Session ID is returned in the query parameter when your customer
-        // is redirected to the success page.
-        success_url: "https://packagescry.com/payment-success",
-        cancel_url: "https://packagescry.com/canceled",
-      })
-
-      return session.url
-    } catch (e) {
-      const error = e as CommonError
-      console.log("Stripe `createCustomer` error", error)
-
-      throw { message: error.message, type: "STRIPE_CREATE_CHECKOUT" }
-    }
-  },
-  getPortalLink: async (idUser: typeof ObjectId) => {
-    const subscription = await stripe.subscriptions.search({
-      query: `metadata['idUser']:'${idUser}'`,
+      },
+      // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+      // the actual Session ID is returned in the query parameter when your customer
+      // is redirected to the success page.
+      success_url: "https://packagescry.com/payment-success",
+      cancel_url: "https://packagescry.com/canceled",
     })
 
-    const { customer, id: idSubscription } = subscription?.data?.[0]
-    const idCustomer = typeof customer === "string" ? customer : customer.id
+    return session.url
+  } catch (e) {
+    const error = e as CommonError
+    console.log("Stripe `createCustomer` error", error)
 
-    if (!idSubscription)
-      throw { message: "No subscription", type: "STRIPE_GET_PORTAL_LINK" }
+    throw { message: error.message, type: "STRIPE_CREATE_CHECKOUT" }
+  }
+}
 
-    const portalSession = await stripe.billingPortal.sessions.create({
-      customer: idCustomer,
-    })
+export const getPortalLink = async (idUser: typeof ObjectId) => {
+  const subscription = await stripe.subscriptions.search({
+    query: `metadata['idUser']:'${idUser}'`,
+  })
 
-    return portalSession.url
-  },
-  getStripeSubscription: async (idSubscription: ObjectId) => {
-    try {
-      const subscription = await stripe.subscriptions.retrieve(
-        idSubscription.toString()
-      )
+  const { customer, id: idSubscription } = subscription?.data?.[0]
+  const idCustomer = typeof customer === "string" ? customer : customer.id
 
-      console.log("sub")
-      console.log(subscription)
+  if (!idSubscription)
+    throw { message: "No subscription", type: "STRIPE_GET_PORTAL_LINK" }
 
-      return {
-        idSubscription: subscription.id,
-        period:
-          subscription.items.data[0].price.id === STRIPE_YEARLY_ID
-            ? "annual"
-            : "monthly",
-        status: subscription.status,
-      }
-    } catch (e) {
-      const error = e as CommonError
-      throw { message: error.message, type: "STRIPE_GET_SUBSCRIPTION" }
+  const portalSession = await stripe.billingPortal.sessions.create({
+    customer: idCustomer,
+  })
+
+  return portalSession.url
+}
+
+export const getStripeSubscription = async (idSubscription: ObjectId) => {
+  try {
+    const subscription = await stripe.subscriptions.retrieve(
+      idSubscription.toString()
+    )
+
+    console.log("sub")
+    console.log(subscription)
+
+    return {
+      idSubscription: subscription.id,
+      period:
+        subscription.items.data[0].price.id === STRIPE_YEARLY_ID
+          ? "annual"
+          : "monthly",
+      status: subscription.status,
     }
-  },
-  createEvent: (req: Request, res: Response) => {
-    try {
-      return stripe.webhooks.constructEvent(
-        req.body,
-        req.headers["stripe-signature"] ?? "",
-        process.env.STRIPE_WEBHOOK_SECRET || ""
-      )
-    } catch (e) {
-      const error = e as CommonError
+  } catch (e) {
+    const error = e as CommonError
+    throw { message: error.message, type: "STRIPE_GET_SUBSCRIPTION" }
+  }
+}
 
-      console.log(error)
-      console.log(`⚠️  Webhook signature verification failed.`)
-      console.log(
-        `⚠️  Check the env file and enter the correct webhook secret.`
-      )
-      res.status(400).send(`Webhook Error: ${error.message}`)
-    }
-  },
+export const createEvent = (req: Request, res: Response) => {
+  try {
+    return stripe.webhooks.constructEvent(
+      req.body,
+      req.headers["stripe-signature"] ?? "",
+      process.env.STRIPE_WEBHOOK_SECRET || ""
+    )
+  } catch (e) {
+    const error = e as CommonError
+
+    console.log(error)
+    console.log(`⚠️  Webhook signature verification failed.`)
+    console.log(`⚠️  Check the env file and enter the correct webhook secret.`)
+    res.status(400).send(`Webhook Error: ${error.message}`)
+  }
 }
