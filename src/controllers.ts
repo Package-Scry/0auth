e4c0a791dc19b95fcae2254a641928eb4bde2ada
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-07-31 22:00:49 +0200

    convert  controller to es6 exports

--- src/controllers.ts
+++ src/controllers.ts
@@ -6,7 +6,7 @@ type UserQuery = {
   _id: ObjectId
 }
 
-const getCurrentUser = async (query: UserQuery) => {
+export const getCurrentUser = async (query: UserQuery) => {
   try {
     const database = client.db("website")
     const admins = database.collection("strapi_administrator")
@@ -23,69 +23,66 @@ const getCurrentUser = async (query: UserQuery) => {
   }
 }
 
-module.exports = {
-  getCurrentUser,
-  createNewUser: async (idGitHub: string, username: string) => {
+export const createNewUser = async (idGitHub: string, username: string) => {
+  const database = client.db("website")
+  const users = database.collection("users-permissions_user")
+
+  const newUser = {
+    idGitHub,
+    createdAt: new Date(),
+    hasPro: true,
+    lastPaidAt: new Date(),
+    // strapi fields
+    username,
+    password: "",
+    confirmed: true,
+    blocked: false,
+    provider: "local",
+    created_by: new ObjectId("605b9add8e10cc7b4c37930a"),
+    role: new ObjectId("605b9a5d8e10cc7b4c379234"),
+    updated_by: new ObjectId("605b9add8e10cc7b4c37930a"),
+  }
+
+  try {
+    const { insertedId } = await users.insertOne(newUser)
+
+    return await getCurrentUser({ _id: insertedId })
+  } catch (error) {
+    console.error("Mongo user insert error:", error)
+  }
+}
+export const updateUser = async (user: {
+  id: ObjectId
+  hasPro: boolean
+  period: null | PlanPeriods
+}) => {
+  const { id, ...userWithoutId } = user
+  const database = client.db("website")
+  const users = database.collection("users-permissions_user")
+
+  console.log("updating user", id)
+  console.log("updating user", userWithoutId)
+
+  try {
+    await users.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { ...userWithoutId } }
+    )
+  } catch (error) {
+    console.error("Mongo user update error:", error)
+  }
+}
+export const isAdmin = async (id: string) => {
+  try {
     const database = client.db("website")
-    const users = database.collection("users-permissions_user")
+    const admins = database.collection("strapi_administrator")
+    const admin = await admins.findOne({ _id: new ObjectId(id) })
 
-    const newUser = {
-      idGitHub,
-      createdAt: new Date(),
-      hasPro: true,
-      lastPaidAt: new Date(),
-      // strapi fields
-      username,
-      password: "",
-      confirmed: true,
-      blocked: false,
-      provider: "local",
-      created_by: new ObjectId("605b9add8e10cc7b4c37930a"),
-      role: new ObjectId("605b9a5d8e10cc7b4c379234"),
-      updated_by: new ObjectId("605b9add8e10cc7b4c37930a"),
-    }
+    return !!admin
+  } catch (e) {
+    console.error("isAdmin")
+    console.error(e)
 
-    try {
-      const { insertedId } = await users.insertOne(newUser)
-
-      return await getCurrentUser({ _id: insertedId })
-    } catch (error) {
-      console.error("Mongo user insert error:", error)
-    }
-  },
-  updateUser: async (user: {
-    id: ObjectId
-    hasPro: boolean
-    period: null | PlanPeriods
-  }) => {
-    const { id, ...userWithoutId } = user
-    const database = client.db("website")
-    const users = database.collection("users-permissions_user")
-
-    console.log("updating user", id)
-    console.log("updating user", userWithoutId)
-
-    try {
-      await users.updateOne(
-        { _id: new ObjectId(id) },
-        { $set: { ...userWithoutId } }
-      )
-    } catch (error) {
-      console.error("Mongo user update error:", error)
-    }
-  },
-  isAdmin: async (id: string) => {
-    try {
-      const database = client.db("website")
-      const admins = database.collection("strapi_administrator")
-      const admin = await admins.findOne({ _id: new ObjectId(id) })
-
-      return !!admin
-    } catch (e) {
-      console.error("isAdmin")
-      console.error(e)
-
-      return false
-    }
-  },
+    return false
+  }
 }
